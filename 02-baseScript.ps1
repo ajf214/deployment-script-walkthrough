f924ab279f129a0f4d3ff5f90940f181775c3996
alfran@microsoft.com <alfran@microsoft.com>
2019-06-23 20:53:53 -0700

    initial pass at walkthrough

---
+++ 02-baseScript.ps1
@@ -0,0 +1,11 @@
+$apiKey = $someSecret
+
+# do operation to get address prefix
+$Cred = Get-Credential # not sure how this works
+$Url = 'https://server.contoso.com:8089/new-ip-range' # can't use double quotes with inline script..
+$Body = @{}
+
+$result = Invoke-RestMethod -Method 'Post' -Uri $url -Credential $Cred -Body $body
+
+$addressPrefix = $result.newAddressRange
+$subnetPrefix = $result.newSubnetRange
