09ed32490c40774988409a3ecb70535d62048073
alfran@microsoft.com <alfran@microsoft.com>
2019-06-23 22:08:47 -0700

    added question about secrets

--- 02-baseScript.ps1
+++ 02-baseScript.ps1
@@ -1,12 +1,12 @@
+# how do I get "someSecret"? What is the correct way to pass a secret?
 $apiKey = $someSecret
 
 # do operation to get address prefix
-$Cred = Get-Credential # not sure how this works
 # can't use double quotes with inline script..
 $Url = 'https://server.contoso.com:8089/new-ip-range' 
 $Body = @{}
 
-$result = Invoke-RestMethod -Method 'Post' -Uri $url -Credential $Cred -Body $body
+$result = Invoke-RestMethod -Method 'Post' -Uri $url -Credential $apiKey -Body $body
 
 $addressPrefix = $result.newAddressRange
 $subnetPrefix = $result.newSubnetRange
