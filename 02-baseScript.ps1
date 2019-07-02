4822455988b4d9333517def4ac9d93950b59a340
alfran@microsoft.com <alfran@microsoft.com>
2019-07-02 10:00:35 -0700

    updates to secret passing, output details after review with brian

--- 02-baseScript.ps1
+++ 02-baseScript.ps1
@@ -1,12 +1,15 @@
-# how do I get "someSecret"? What is the correct way to pass a secret?
-$apiKey = $someSecret
+param(
+    # this way, on my client when testing, I can not set the environment variable
+    # should work in bash as well
+    [Parameter]$apiKey = $env:SECRET_APIKEY 
+)
 
 # do operation to get address prefix
 # can't use double quotes with inline script..
 $Url = 'https://server.contoso.com:8089/new-ip-range' 
 $Body = @{}
 
-$result = Invoke-RestMethod -Method 'Post' -Uri $url -Credential $apiKey -Body $body
+$result = Invoke-RestMethod -Method 'Post' -Uri $url -Credential $env:SECRET_APIKEY -Body $body
 
 $addressPrefix = $result.newAddressRange
 $subnetPrefix = $result.newSubnetRange
--- 03-addScriptOutput.ps1
+++ 03-addScriptOutput.ps1
@@ -1,14 +1,21 @@
-$apiKey = $someSecret
+param(
+    # this way, on my client when testing, I can set the environment variable
+    # should work in bash as well
+    [Parameter]$apiKey = $env:SECRET_APIKEY 
+)
 
 # do operation to get address prefix
-$Cred = Get-Credential # not sure how this works
-$Url = 'https://server.contoso.com:8089/new-ip-range' # can't use double quotes with inline script..
+# can't use double quotes with inline script..
+$Url = 'https://server.contoso.com:8089/new-ip-range' 
 $Body = @{}
 
-$result = Invoke-RestMethod -Method 'Post' -Uri $url -Credential $Cred -Body $body
+$result = Invoke-RestMethod -Method 'Post' -Uri $url -Credential $apiKey -Body $body
 
 $addressPrefix = $result.newAddressRange
 $subnetPrefix = $result.newSubnetRange
+
+
+# OPEN ISSUE FOR HOW SCRIPT OUTPUTS SHOULD BE HANDLED
 
 <# 
  these probably need a different name as they only pertain to deploymentScript
@@ -16,3 +23,20 @@ $subnetPrefix = $result.newSubnetRange
 #> 
 Add-ScriptOutput('addressRange', $addressPrefix) # (outputName, outputValue)
 Add-ScriptOutput('subnetRange', $subnetPrefix)
+
+
+<#
+requirements:
+- run on client w/o having to reference a specific image
+    * prefer not to have to install a separate module/script for local testing
+- outputs need to be stored on disk as JSON in such a way that it can be read 
+  by the deployment script resoruce 
+    * should look like template outputs
+
+# how AzDO does this...
+# Write-Host "##vso[task.setvariable variable=resourceGroup.name]$resourceGroupName"
+
+chat with powershell folks
+ - who is pm over there...
+
+#>
